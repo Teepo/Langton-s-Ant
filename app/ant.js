8225f6209f294f7e548d70210b84e0792effee56
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2016-07-01 13:48:14 +0200

    ANT: give reference to App, store reference to his <Cell />

--- app/ant.js
+++ app/ant.js
@@ -3,19 +3,30 @@ var Ant = React.createClass({
     getInitialState: function() {
 
         return {
-            x : 5,
-            y : 5,
-            rotation: 0,
-            side: 0
+            x : 50,
+            y : 50,
+            rotation : 0,
+            side : 0,
+            currentCell : this.props.cell
         };
     },
 
     render: function() {
 
-        if (this.props.cell.state.coords.x == this.state.x
-           && this.props.cell.state.coords.y == this.state.y) {}
+        if ((this.state.currentCell.state.coords.x == this.state.x
+           && this.state.currentCell.state.coords.y == this.state.y)
+
+           || this.state.currentCell.state.antIsHere === true)
+        {
+            this.props.App.currentCell = this.state.currentCell;
+            this.props.App.currentCell.state.antIsHere = true;
+
+            this.props.App.ant = this;
+        }
         else
+        {
             return false;
+        }
 
         var __antClassName = "ant _" + this.state.rotation;
 
